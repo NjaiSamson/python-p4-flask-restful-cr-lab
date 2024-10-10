e1e98eb2229a927eb81ac898cc0cd507eef0ab76
Samson Njai <njaisamson@gmail.com>
2024-10-10 19:24:46 +0300

    adds the required RESTful APIs

--- server/app.py
+++ server/app.py
@@ -17,10 +17,38 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    def get(self):
+        plants = Plant.query.all()
+        plants_dict = [plant.to_dict() for plant in plants]
+        
+        response = make_response(plants_dict, 200)
+        return response
 
+    def post(self):
+        data = request.get_json()        
+        new_plant = Plant(
+            name=data.get('name'),
+            image=data.get('image'),
+            price=data.get('price'),            
+        )
+        
+        db.session.add(new_plant)
+        db.session.commit()
+        
+        response_dict = new_plant.to_dict()
+        
+        return jsonify(response_dict), 201  # Returning the created plant with 201 status
+    
+# Adding the resource to the API
+api.add_resource(Plants, '/plants')
 class PlantByID(Resource):
-    pass
+    def get(self, id):
+        plant_by_id = Plant.query.filter(Plant.id == id).first()
+        plant_by_id_dict = plant_by_id.to_dict()
+        
+        response = make_response(plant_by_id_dict, 200,)
+        return response
+api.add_resource(PlantByID, '/plants/<int:id>')
         
 
 if __name__ == '__main__':
